76a3d0315082ff79fb97ad13c1665718904f702a
rlvuddl1234 <rlvuddl1234@naver.com>
2024-10-10 19:57:12 +0900

    1-6 필수 과제

--- src/main/java/org/example/expert/domain/todo/entity/Todo.java
+++ src/main/java/org/example/expert/domain/todo/entity/Todo.java
@@ -30,7 +30,7 @@ public class Todo extends Timestamped {
     @OneToMany(mappedBy = "todo", cascade = CascadeType.REMOVE)
     private List<Comment> comments = new ArrayList<>();
 
-    @OneToMany(mappedBy = "todo")
+    @OneToMany(mappedBy = "todo", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Manager> managers = new ArrayList<>();
 
     public Todo(String title, String contents, String weather, User user) {
@@ -40,4 +40,5 @@ public class Todo extends Timestamped {
         this.user = user;
         this.managers.add(new Manager(user, this));
     }
+    
 }
